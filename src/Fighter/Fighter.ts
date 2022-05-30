8adff1723d4d1268598199a4aa2d1b23e6d8a41e
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-05-29 22:58:13 -0300

    pass requeriment 9

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -4,6 +4,8 @@ import SimpleFighter from './SimpleFighter';
 export default interface Fighter extends SimpleFighter {
   defense: number;
   energy?: Energy;
+
+  attack(enemy: SimpleFighter | Fighter): void;
   special(enemy: Fighter): void;
   levelUp(): void;
 }
